21fb40518a10f0af118911df6535c0ac5ea97c12
DAM <2061784@alu.murciaeduca.es>
2025-02-04 16:10:29 +0100

    de momento

---
+++ app/tabs/login/login.tsx
@@ -0,0 +1,19 @@
+import { View, Text, TextInput, Image, Pressable } from 'react-native';
+import React from 'react'
+import { GlobalStyles } from '../../../theme/GlobalStyles';
+
+import { router } from "expo-router";
+
+
+const login = () => {
+  return (
+    <View>
+        <Text>
+            Hola a todos
+        </Text>
+    </View>
+
+  )
+}
+
+export default login
