04dfae6f91ffdeb29f751c448a9459c9a6d6454b
EricBL28 <2061784@alu.murciaeduca.es>
2025-02-05 21:26:20 +0100

    fin

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { GlobalStyles } from '@/theme/GlobalStyles'
+import { Boton } from '../../components/Boton'
 
 const ScreenPrincipal = () => {
   return (
     <View style={GlobalStyles.containerCentrado}>
-      <Text style= {GlobalStyles.titulo}>Screen Principal</Text>
+
+      <Boton label='Login' width={200} backgroundColor='naranja' link='../login'></Boton>
     </View>
   )
 }
